d0094b4dd327b82a582ce0668a905cf67359f788
Amicuchi <andersonamicuchi@gmail.com>
2021-03-29 11:34:27 -0300

    inclusão de rotas no app.js, na header e index.

--- FCamara_Squad22-main/backend/app.js
+++ FCamara_Squad22-main/backend/app.js
@@ -26,20 +26,11 @@ app.use(express.urlencoded({
 
 app.use(express.static(path.join(__dirname, '../frontend/public')));
 
-
 app.set('view engine', 'ejs');
 
 app.set('views', path.join(__dirname, '../frontend/views'));
 
-app.get('/', (req, res) => {
-    res.render('index');
-});
-
 const estados = ["AC", "AL", "AM", "AP", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RO", "RS", "RR", "SC", "SE", "SP", "TO" ];
-
-app.get('/registrar', (req, res) => {
-    res.render('registrar-aluno', {estados});
-})
 
 app.post('/registrar', async (req, res) => {
     const aluno = new Aluno(req.body.aluno)
@@ -47,26 +38,43 @@ app.post('/registrar', async (req, res) => {
     res.redirect('/');
 })
 
+
+// CONFIRMAÇÃO DE QUE ESTÁ FUNCIONANDO
+app.listen('3000', () => {
+    console.log('Escutando porta 3000');
+});
+
+
+// ROTAS PARA PAGINAS
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
 app.get('/login', (req, res) => {
     res.render('index');
-})
-
-app.get('/pesquisa', (req, res) => {
-    res.render('schoolResults');
-})
-
-app.get('/aluno/doacao/obrigado', (req, res) => {
-    res.render('thanks');
-})
-
-app.get('/dashboard', (req, res) => {
-    res.render('dashboard');
 })
 
 app.get('/doar', (req, res) => {
     res.render('doar');
 })
 
-app.listen('3000', () => {
-    console.log('Escutando porta 3000');
-});
+app.get('/aluno/doacao/obrigado', (req, res) => {
+    res.render('thanks');
+})
+
+app.get('/pesquisa', (req, res) => {
+    res.render('schoolResults');
+})
+
+app.get('/registrar', (req, res) => {
+    res.render('registrar-aluno', {estados});
+})
+
+app.get('/dashboard', (req, res) => {
+    res.render('dashboard');
+})
+
+
+
+
+
