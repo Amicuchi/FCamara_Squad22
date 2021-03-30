38b499a84f94befa6d3e7603ef664b8a95ab5dae
Pedro Pataro <phsilvino.7789@hotmail.com>
2021-03-30 17:45:53 -0300

    Validação de login

--- FCamara_Squad22-main/backend/app.js
+++ FCamara_Squad22-main/backend/app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+
 const ExpressError = require('./utils/ExpressError');
+const Aluno = require('./models/aluno');
 const indexRoutes = require('./routes/index');
 const alunoRoutes = require('./routes/aluno');
+
 const app = express();
 
 require('dotenv').config();
@@ -12,24 +18,43 @@ const databaseUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/UeP';
 
 mongoose.connect(databaseUri, {
         useNewUrlParser: true,
-        useUnifiedTopology: true
+        useUnifiedTopology: true,
+        useCreateIndex: true
     })
     .then(() => {
-        console.log('Conexão Aberta');
+        console.log('Conexão com DB Aberta');
     })
     .catch((err) => {
         console.log('Erro na conexão');
         console.log(err);
     });
 
+const sessionConfig = {
+    secret: 'thisshouldbeabettersecret!',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+
 app.use(express.urlencoded({
     extended: true
 }));
-
 app.use(express.static(path.join(__dirname, '../frontend/public')));
+app.use(session(sessionConfig));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(Aluno.createStrategy());
+
+passport.serializeUser(Aluno.serializeUser());
+passport.deserializeUser(Aluno.deserializeUser());
 
 app.set('view engine', 'ejs');
-
 app.set('views', path.join(__dirname, '../frontend/views'));
 
 // ROTAS PARA PAGINAS
