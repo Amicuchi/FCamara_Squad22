0c80097dc198a0b0c68d708940f243c13ac1d6d0
Pedro Pataro <phsilvino.7789@hotmail.com>
2021-04-02 18:22:04 -0300

    função de pesquisa e exibir resultados

--- FCamara_Squad22-main/backend/routes/index.js
+++ FCamara_Squad22-main/backend/routes/index.js
@@ -3,6 +3,7 @@ const passport = require('passport');
 const indexRouter = express.Router();
 const { isLoggedIn } = require('../middleware/isLoggedIn');
 const Aluno = require('../models/aluno');
+const ListaMaterial = require('../models/listaMaterial');
 const catchAsync = require('../utils/catchAsync');
 
 indexRouter.get('/', (req, res) => {
@@ -17,17 +18,32 @@ indexRouter.post('/login', passport.authenticate('local', {failureRedirect: '/lo
     res.redirect('/dashboard');
 });
 
-indexRouter.get('/pesquisa', (req, res) => {
-    res.render('schoolResults');
-});
-
 indexRouter.get('/dashboard', isLoggedIn, (req, res) => {
     const {nomeResponsavel} = req.user;
     res.render('dashboard', {nomeResponsavel});
 });
 
-indexRouter.get('/doar', (req, res) => {
-    res.render('doar');
-});
+indexRouter.get('/doar', catchAsync(async(req, res) => {
+    const alunos = await Aluno.find({})
+    res.render('doar', {alunos});
+}));
+
+indexRouter.get('/pesquisa', catchAsync(async(req, res) => {
+    const {qType, q} = req.query;
+    if (qType === 'escola') {
+        const alunos = await Aluno.find({escola : { $regex: q}}).populate('listaMateriais');
+        return res.render('schoolResults', {alunos})
+    } else if (qType === 'tipoMaterial') {
+        const material = await ListaMaterial.find({$or: [
+            {tipoMaterial: {$regex: q}},
+            {materialDesc: {$regex: q}}
+        ]});
+        for (m of material) {
+            const alunos = await Aluno.find({listaMateriais: {$in: m._id}}).populate('listaMateriais');
+            console.log(alunos)
+            return res.render('schoolResults', {alunos})
+        }
+    }
+}))
 
 module.exports = indexRouter;
